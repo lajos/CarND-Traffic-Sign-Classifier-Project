7164becbef701b42108c40fb5891a59dd38b503f
lajos <panka.nospam@gmail.com>
2017-04-11 22:08:05 -0400

    first training tests

--- ts-preprocess.py
+++ ts-preprocess.py
@@ -1,3 +1,15 @@
+import time
+start_time = time.time()
+
+DISABLE_GPU = False
+import os
+if DISABLE_GPU:
+    os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+else:
+    os.environ["CUDA_VISIBLE_DEVICES"] = ""
+
+from tensorflow.examples.tutorials.mnist import input_data
+
 #---------------------------------------------------------------------
 
 # Load pickled data
@@ -17,6 +29,8 @@ with open(testing_file, mode='rb') as f:
 X_train, y_train = train['features'], train['labels']
 X_valid, y_valid = valid['features'], valid['labels']
 X_test, y_test = test['features'], test['labels']
+
+print("--- loading: %s seconds ---" % (time.time() - start_time))
 
 #---------------------------------------------------------------------
 
@@ -94,6 +108,7 @@ import numpy as np
 import skimage.transform
 import skimage.morphology
 import skimage.filters.rank
+import skimage.exposure
 import random
 import warnings
 
@@ -134,7 +149,7 @@ def img_equalize_clahe(img, clipLimit=4, tileSize=14):
 def img_equalize_adapthist(img):
     with warnings.catch_warnings():
         warnings.simplefilter("ignore")
-        img = exposure.equalize_adapthist(img)
+        img = skimage.exposure.equalize_adapthist(img)
     return(img)
 
 def img_median(img, size=5):
@@ -199,51 +214,104 @@ def fully_connected(input, n_output):
     return tf.add(tf.matmul(input, weights), biases)
 
 def fully_connected_relu(input, n_output):
-    tf.nn.relu(fully_connected(input, n_output))
+    return tf.nn.relu(fully_connected(input, n_output))
 
 #---------------------------------------------------------------------
 
-def training_model(x):
+
+def lenet_model(x):
     with tf.variable_scope('cr1'):
-        o = convolution_relu(x, K=5, S=1, D=6, padding='SAME')
-        print('c1 shape:', o.shape)
-        o = pool(o, 2)
+        cr1 = convolution_relu(x, K=5, S=1, D=6, padding='SAME')
+        print('cr1 shape:', cr1.shape)
+        cr1 = pool(cr1, 2)
 
     with tf.variable_scope('cr2'):
-        o = convolution_relu(o, K=5, S=1, D=16, padding='SAME')
-        print('c2 shape:', o.shape)
-        o = pool(o, 2)
+        cr2 = convolution_relu(cr1, K=5, S=1, D=16, padding='SAME')
+        print('cr2 shape:', cr2.shape)
+        cr2 = pool(cr2, 2)
 
-    o = flatten(o)
+    flat = flatten(cr2)
+    print('flat shape:', flat.shape)
 
     with tf.variable_scope('fcr1'):
-        o = fully_connected_relu(o, 120)
-        print('fc1 shape:', o.shape)
+        fcr1 = fully_connected_relu(flat, 120)
+        print('fcr1 shape:', fcr1.shape)
 
     with tf.variable_scope('fcr2'):
-        o = fully_connected_relu(o, 84)
-        print('fc2 shape:', o.shape)
+        fcr2 = fully_connected_relu(fcr1, 84)
+        print('fcr2 shape:', fcr2.shape)
 
     with tf.variable_scope('fc3'):
-        o = fully_connected(o, 43)
-        print('fc3 shape:', o.shape)
+        fc3 = fully_connected(fcr2, 43)
+        print('fc3 shape:', fc3.shape)
 
-    return(o)
+    logits = fc3
 
-#X_train_norm = np.array([img_rgb2LNorm(xi) for xi in X_train])
-#X_valid_norm = np.array(img_[rgb2LNorm(xi) for xi in X_train])
-#X_test_norm = np.array(img_[rgb2LNorm(xi) for xi in X_train])
-#y_train_onehot = tf.one_hot(y_train, 10)
+    return(fc3)
+
+
+def training_model(x):
+    with tf.variable_scope('cr1'):
+        cr1 = convolution_relu(x, K=5, S=1, D=32, padding='SAME')
+        print('cr1 shape:', cr1.shape)
+        cr1 = pool(cr1, 2)
+
+    with tf.variable_scope('cr2'):
+        cr2 = convolution_relu(cr1, K=5, S=1, D=64, padding='SAME')
+        print('cr2 shape:', cr2.shape)
+        cr2 = pool(cr2, 2)
+
+    with tf.variable_scope('cr3'):
+        cr3 = convolution_relu(cr2, K=5, S=1, D=128, padding='SAME')
+        print('cr3 shape:', cr3.shape)
+        cr3 = pool(cr3, 2)
+
+
+    flat = tf.concat((flatten(cr1),flatten(cr2),flatten(cr3)),1)
+    print('flat shape:', flat.shape)
+
+    with tf.variable_scope('fcr1'):
+        fcr1 = fully_connected_relu(flat, 2048)
+        print('fcr1 shape:', fcr1.shape)
+
+    with tf.variable_scope('fcr2'):
+        fcr2 = fully_connected_relu(fcr1, 512)
+        print('fcr2 shape:', fcr2.shape)
+
+    with tf.variable_scope('fc3'):
+        fc3 = fully_connected(fcr2, 43)
+        print('fc3 shape:', fc3.shape)
+
+    logits = fc3
+
+    return(fc3)
+
+X_train_norm = np.array([img_equalize_adapthist(img_rgb2LNorm(xi)) for xi in X_train])
+X_valid_norm = np.array([img_equalize_adapthist(img_rgb2LNorm(xi)) for xi in X_valid])
+X_test_norm = np.array([img_equalize_adapthist(img_rgb2LNorm(xi)) for xi in X_test])
+
+print("--- data norm: %s seconds ---" % (time.time() - start_time))
+
+X_train = X_train_norm[:,:,:,None]
+X_validation = X_valid_norm[:,:,:,None]
+X_test = X_test_norm[:,:,:,None]
+
+y_validation = y_valid
+
+from sklearn.utils import shuffle
+X_train, y_train = shuffle(X_train, y_train)
 
 x = tf.placeholder(tf.float32, (None, image_w, image_h, 1))
 y = tf.placeholder(tf.int32, (None))
 y_one_hot = tf.one_hot(y, n_classes)
 
-rate = 0.001
+EPOCHS = 20
+BATCH_SIZE = 256
+rate = 0.0005
 
 logits = training_model(x)
 
-cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_train_onehot, logits=logits)
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=y_one_hot, logits=logits)
 loss_operation = tf.reduce_mean(cross_entropy)
 optimizer = tf.train.AdamOptimizer(learning_rate = rate)
 training_operation = optimizer.minimize(loss_operation)
@@ -252,3 +320,45 @@ correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(y_one_hot, 1))
 accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 saver = tf.train.Saver()
 
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+
+    print("Training...")
+    print()
+    for i in range(EPOCHS):
+        X_train, y_train = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train[offset:end], y_train[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+
+        validation_accuracy = evaluate(X_validation, y_validation)
+        print("EPOCH {} ...".format(i+1))
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        print()
+
+    print("--- training: %s seconds ---" % (time.time() - start_time))
+
+    saver.save(sess, './lenet')
+    print("Model saved")
+
+print("--- saving: %s seconds ---" % (time.time() - start_time))
+
+with tf.Session() as sess:
+    saver.restore(sess, tf.train.latest_checkpoint('.'))
+
+    test_accuracy = evaluate(X_test, y_test)
+    print("Test Accuracy = {:.3f}".format(test_accuracy))
+
+print("--- total: %s seconds ---" % (time.time() - start_time))
